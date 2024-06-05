27549436644009b0b5bdac890b64c968733c3240
Billy Hopkinson-Wood <billy@bhwood.dev>
2024-06-05 15:11:29 +0100

    remove erroneous imports

--- api_handler.py
+++ api_handler.py
@@ -1,18 +1,16 @@
 import requests
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-uprn = os.getenv("UPRN")
-
 
 def get_collection_data(uprn):
     url = "https://www.wealden.gov.uk/wp-admin/admin-ajax.php"
 
-    headers = {}
+    headers = {
+    }
 
-    data = {"action": "wealden_get_collections_for_uprn", "uprn": uprn}
+    data = {
+        'action': 'wealden_get_collections_for_uprn',
+        'uprn': uprn
+    }
+
 
     response = requests.post(url, headers=headers, data=data)
     return response
